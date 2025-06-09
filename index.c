9d7a2fa7df60f9e5b804193ae1b03227f7584886
소보로빵 <68942200+soborocat@users.noreply.github.com>
2025-06-09 10:13:22 +0900

    Update index.c

--- index.c
+++ index.c
@@ -8,7 +8,8 @@
 #define HME_POS 1
 #define BWL_PO (ROOM_WIDTH - 2)
 
-int main(void) {
+int main(void)
+{
     printf("****야옹이와 수프****\n\n");
     printf("      /\\_/\\\n");
     printf(" /\\  / o o \\\n");
@@ -29,18 +30,30 @@ int main(void) {
     printf("야옹이의 이름은 %s 입니다.\n", name);
     Sleep(1000);
     system("cls");
-    while (1) {
+    while (1)
+    {
 
         printf("================ 현재 상태 ================\n");
         printf("현재까지 만든 수프: %d개\n", soupCount);
         printf("집사와의 관계(0~4): %d\n", friendshipPoint);
 
-        switch (friendshipPoint) {
-        case 0: printf("곁에 오는 것조차 싫어합니다.\n");break;
-        case 1: printf("간식 자판기 취급입니다.\n"); break;
-        case 2: printf("그럭저럭 쓸 만한 집사입니다.\n"); break;
-        case 3: printf("훌륭한 집사로 인정 받고 있습니다.\n"); break;
-        case 4: printf("집사 껌딱지입니다.\n"); break;
+        switch (friendshipPoint)
+        {
+        case 0:
+            printf("곁에 오는 것조차 싫어합니다.\n");
+            break;
+        case 1:
+            printf("간식 자판기 취급입니다.\n");
+            break;
+        case 2:
+            printf("그럭저럭 쓸 만한 집사입니다.\n");
+            break;
+        case 3:
+            printf("훌륭한 집사로 인정 받고 있습니다.\n");
+            break;
+        case 4:
+            printf("집사 껌딱지입니다.\n");
+            break;
         }
 
         printf("===========================================\n\n");
@@ -53,35 +66,45 @@ int main(void) {
 
         printf("%d이(가) 나왔습니다!\n", dice);
 
-        if (dice >= 6 - friendshipPoint ) {
-            if (CPosX < BWL_PO - 1) {
+        if (dice >= 6 - friendshipPoint)
+        {
+            if (CPosX < BWL_PO - 1)
+            {
                 printf("냄비 쪽으로 움직입니다.\n\n");
                 CPosX++;
             }
         }
-        else {
-            if (CPosX > 0) {
+        else
+        {
+            if (CPosX > 0)
+            {
                 printf("집 쪽으로 움직입니다.\n\n");
                 CPosX--;
             }
         }
         Sleep(500);
 
-        if (CPosX == 0) {
+        if (CPosX == 0)
+        {
             printf("%s은(는) 자신의 집에서 편안함을 느낍니다.\n\n", name);
         }
-        else {
+        else
+        {
 
-            if (CPosX == BWL_PO - 1) {
+            if (CPosX == BWL_PO - 1)
+            {
                 soupCount++;
                 int SoupType = rand() % 3;
-                if (SoupType == 0) {
+                if (SoupType == 0)
+                {
                     printf("%s이(가) 감자 수프를 만들었습니다!\n\n", name);
                 }
-                else if (SoupType == 1) {
+                else if (SoupType == 1)
+                {
                     printf("%s이(가) 양송이 수프를 만들었습니다!\n\n", name);
                 }
-                else if (SoupType == 2) {
+                else if (SoupType == 2)
+                {
                     printf("%s이(가) 브로콜리 수프를 만들었습니다!\n\n", name);
                 }
                 printf("현재까지 만든 수프: %d개\n", soupCount);
@@ -90,36 +113,48 @@ int main(void) {
         Sleep(500);
 
         // Render Room
-        for (int i = 0; i < ROOM_WIDTH; i++) {
+        for (int i = 0; i < ROOM_WIDTH; i++)
+        {
             printf("#");
         } // 0 Brick
         printf("\n#");
-        for (int i = 0; i < BWL_PO; i++) {
-            if (i == 0) {
+        for (int i = 0; i < BWL_PO; i++)
+        {
+            if (i == 0)
+            {
                 printf("H");
             }
-            else if (i == BWL_PO - 1) {
+            else if (i == BWL_PO - 1)
+            {
                 printf("B");
             }
-            else {
+            else
+            {
                 printf(" ");
             }
         } // 1 Items
         printf("#");
 
         printf("\n#");
-        for (int i = 0; i < BWL_PO; i++) {
-            if (i == CPosX) {
+        for (int i = 0; i < BWL_PO; i++)
+        {
+            if (i == CPosX)
+            {
                 printf("C");
-            } else if (i == prevX) {
+            }
+            else if (i == prevX)
+            {
                 printf(".");
-            } else {
+            }
+            else
+            {
                 printf(" ");
             }
         } // 2 Cat
         printf("#\n");
 
-        for (int i = 0; i < ROOM_WIDTH; i++) {
+        for (int i = 0; i < ROOM_WIDTH; i++)
+        {
             printf("#");
         } // 3 Brick
         printf("\n\n");
@@ -130,24 +165,30 @@ int main(void) {
         Sleep(500);
 
         printf("어떤 상호작용을 하시겠습니까?\t0. 아무것도 하지 않음.\t1. 긁어 주기\n");
-        while (1) {
+        while (1)
+        {
             printf(">> ");
             scanf("%d", &select);
-            if (0 <= select && select <= 1) {
+            if (0 <= select && select <= 1)
+            {
                 break;
             }
         }
-        switch (select) {
+        switch (select)
+        {
         case 0:
             printf("아무것도 하지 않습니다.\n4/6의 확률로 친밀도가 떨어집니다.\n");
             dice = rand() % 5 + 1;
             printf("%d이(가) 나왔습니다!\n", dice);
 
-            if (dice <= 4) {
+            if (dice <= 4)
+            {
                 printf("친밀도가 떨어집니다.\n");
-                if (friendshipPoint > 0) friendshipPoint--;
+                if (friendshipPoint > 0)
+                    friendshipPoint--;
             }
-            else {
+            else
+            {
                 printf("다행히 친밀도가 떨어지지 않았습니다.\n");
             }
 
@@ -160,16 +201,18 @@ int main(void) {
 
             dice = rand() % 5 + 1;
             printf("%d이(가) 나왔습니다!\n", dice);
-            if (dice <= 2) {
+            if (dice <= 2)
+            {
                 printf("친밀도가 높아집니다.\n");
-                if (friendshipPoint < 4) friendshipPoint++;
+                if (friendshipPoint < 4)
+                    friendshipPoint++;
             }
-            else {
+            else
+            {
                 printf("친밀도가 그대로입니다.\n");
                 printf("현재 친밀도: %d\n", friendshipPoint);
             }
             break;
-
         }
         prevX = CPosX;
         Sleep(2500);
